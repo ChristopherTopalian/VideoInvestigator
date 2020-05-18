c07c5c76ac9eb3cce390a6ce947486c7096ff089
Christopher Topalian <58909034+ChristopherTopalian@users.noreply.github.com>
2020-05-17 22:06:10 -0400

    Add files via upload

---
+++ record.js
@@ -0,0 +1,1 @@
+records=[ { "recordId": 0, "start": 27, "end": 29, "startMins": "00:00:27", "endMins": "00:00:29", "note": "Double Shoulder Shrug", "words": "I'm his office manager. Maybe I can help.", "conclusion": "Body language possibly indicating that she is NOT willing to help" } ]; 
